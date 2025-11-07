59e8be03d922f7e099275db3071737c5f3f03e18
jenny-land <jlanders418@insite.4cd.edu>
2025-11-06 21:33:58 -0800

    added a git ignore file

---
+++ javascript/text-content.js
@@ -0,0 +1,8 @@
+/*
+notes:
+remove skip to Chapter.
+add start button to interactive zone
+change our story to Title
+have intro scene script show on play or start button
+create new script for opening before play
+*/
